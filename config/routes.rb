1323ed2f911f960f332b55c6538a870a63ece5c8
Felipe <fdeodato11@gmail.com>
2019-10-22 00:31:33 -0300

    Adicionada rota para index
    Signed-off-by: Felipe <fdeodato11@gmail.com>

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'home/index'
+   root 'home#index'
   get 'usuario/usuario'
   resources :user
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
